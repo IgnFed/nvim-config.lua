8a106ea7adcc2977ec7c557833991bf5253af632
IgnFed <ignaciofedorenco2317@gmail.com>
2022-10-29 17:52:36 -0300

    feat: formatter and linter for GO

--- lua/plugins/null-lsp.lua
+++ lua/plugins/null-lsp.lua
@@ -1,6 +1,7 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup{
   debug = false,
@@ -9,6 +10,8 @@ null_ls.setup{
      formatting.eslint,
      formatting.stylua.with({extra_args = {"--fast"}}),
      formatting.goimports,
+     formatting.gofmt,
+     diagnostics.golangci_lint
     },
 }
 
